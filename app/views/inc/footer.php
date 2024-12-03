0a339ba7fd4f95800eb9cf2aa15b5f735679620e
omy747 <git_hub_user993@outlook.com>
2024-12-03 21:14:55 +0700

    footer
    Добавлен футер и jquery

---
+++ app/views/inc/footer.php
@@ -0,0 +1,6 @@
+</div>
+<!-- /.container -->
+<script src="<?php echo URLROOT; ?>/js/jquery-3.6.0.min.js"></script>
+<script src="<?php echo URLROOT; ?>/js/bootstrap.min.js"></script>
+</body>
+</html>

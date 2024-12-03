bedd624412c5a5578835924b94c1bc76237f9ca5
omy747 <git_hub_user993@outlook.com>
2024-12-03 21:53:49 +0700

    index
    Добавление изображения на стартовую страницу

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -3,4 +3,14 @@
 <p class="lead text-center">
 <?php echo $data['description']; ?>
 </p>
+
+<div class="row">
+    <div class="col-12 text-center">
+        <img class="w-50" src="<?php echo URLROOT.'/public/images/bg.png'; ?>" alt="">
+    </div>
+    <!-- /.col-12 text-center -->
+</div>
+<!-- /.row -->
+
+
 <?php require APPROOT.'/views/inc/footer.php'; ?>

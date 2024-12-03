ad5e702809835399702922ca0b2e6b2bc2bfde11
omy747 <git_hub_user993@outlook.com>
2024-12-03 19:44:40 +0700

    Pages
    Добавлен конструктор и метод, вызываемый при открытии приложения

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,5 +1,22 @@
 <?php
 class Pages extends Controller {
-
-
+    public function __construct() {   }
+    
+    public function index(){ // запуск стартовой страницы приложения
+        if(isLoggedIn())   {
+           if($_SESSION['user_role'] == "admin"){
+         redirect('dashboard/index');
+     }  elseif($_SESSION['user_role'] == "emp")  {
+         redirect('emps/index'); 
+     }   else {
+         redirect('pages/index');  
+     }
+    }   else   {
+           $data = [
+              'title' => '',
+              'description' => ''    
+           ];       
+           $this->view('pages/index', $data);
+        }   
+     }
 }

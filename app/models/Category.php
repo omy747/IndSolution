8b79b03555a1b54283eb59c34f12ace4c187dea2
omy747 <git_hub_user993@outlook.com>
2024-12-06 10:26:39 +0700

    Cat
    Добавление метода по выборке записей к модели Категория

--- app/models/Category.php
+++ app/models/Category.php
@@ -5,4 +5,19 @@ public function __construct(){
     $this->db = new Database;
 }
 
+public function getCats($param) {
+    $results = null;
+       if($param == null)
+       {
+       $this->db->query("SELECT * FROM categories");
+       $results = $this->db->resultSet();
+       } 
+       else
+       {
+       $this->db->query("SELECT * FROM categories WHERE cat_title like '%".$param."%' or cat_desc like '%".$param."%'");
+       $results = $this->db->resultSet();
+       }       
+       return $results;
+   }
+
 }

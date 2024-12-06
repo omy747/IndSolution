91f2d4052e79249b90a998312c1f5ece046bd1f6
omy747 <git_hub_user993@outlook.com>
2024-12-06 11:26:18 +0700

    prod
    Список с решениями (модель)

--- app/models/Product.php
+++ app/models/Product.php
@@ -5,4 +5,19 @@ public function __construct(){
     $this->db = new Database;
 }
 
+public function getProds($param) {
+    $results = null;
+       if($param == null)
+       {
+       $this->db->query("SELECT * FROM products p, categories c where p.cat_id=c.catId");
+       $results = $this->db->resultSet();
+       } 
+       else
+       {
+       $this->db->query("SELECT * FROM products p, categories c WHERE p.cat_id=c.catId and title like '%".$param."%' or descr like '%".$param."%'");
+       $results = $this->db->resultSet();
+       }       
+       return $results;
+   }
+
 }
